7de9b611693110faab588fce18484088e8b8a978
aleekhabib <aleekhabib@gmail.com>
2022-04-19 21:56:09 +0700

    fix

--- app/Patners/Bri.php
+++ app/Patners/Bri.php
@@ -3,18 +3,18 @@
 namespace App\Patners;
 
 use Illuminate\Support\Facades\Http;
-
+use App\Constants\Snap;
 class Bri {
 
     const host = 'https://sandbox.partner.api.bri.co.id';
     public static function getAccessToken($param)
     {
-        dd($param);
+        // dd($param['id_key']);
         try {
             $response = Http::timeout(5)
             ->withHeaders([
+                'X-CLIENT-KEY' => 'h5KQRxkbd5PxQXD8QRgtf7PvLSsKqbuq',
                 'X-SIGNATURE' => $param['signature'],
-                'X-CLIENT-KEY' => $param['id_key'],
                 'X-TIMESTAMP' => $param['timestamp']
             ])
             ->contentType("application/json")
