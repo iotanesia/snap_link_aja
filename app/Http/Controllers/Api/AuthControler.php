695aac3ce3f90dd1d60fa24ade937625397dd88c
aleekhabib <aleekhabib@gmail.com>
2022-04-25 01:07:05 +0700

    delete chilkat

--- app/Http/Controllers/Api/AuthControler.php
+++ app/Http/Controllers/Api/AuthControler.php
@@ -7,12 +7,10 @@ use Illuminate\Http\Request;
 use App\ApiHelper as ResponseInterface;
 use App\Services\User as Service;
 use App\Services\Signature;
-use App\Helper\Chilkat\Config as Chilkat;
 use Illuminate\Support\Facades\File;
 
 class AuthControler extends Controller
 {
-    use Chilkat;
     public function login(Request $request)
     {
         return ResponseInterface::responseData(
