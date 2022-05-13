bccb8c64463810568a5ee5cce4c2dadede345e56
rexy siwabessy <rexysiwabessy@gmail.com>
2022-05-13 09:45:31 +0700

    updae

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Api\UserControler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
-
+5
 
 /*
 |--------------------------------------------------------------------------
