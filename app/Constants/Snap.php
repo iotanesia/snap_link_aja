448e22031e76b1173cebc2677f6caaa84213b001
aleekhabib <aleekhabib@gmail.com>
2022-05-18 15:13:46 +0700

    change : cliet id mandiri

--- app/Constants/Snap.php
+++ app/Constants/Snap.php
@@ -7,8 +7,8 @@ class Snap
 {
     const CLIENT_ID_BRI = 'h5KQRxkbd5PxQXD8QRgtf7PvLSsKqbuq';
     const CLIENT_SECRET_BRI = '362pXyfC8ApKKjRW';
-    const CLIENT_ID_MANDIRI = 'ce348822-44d8-4000-a1ae-a6c23f27d428';
-    const CLIENT_SECRET_MANDIRI = 'a96dd335-e2ff-4a24-8d17-29749ef35bea';
+    const CLIENT_ID_MANDIRI = '04352571-16f9-4ee8-a654-b6ae06bd8ec6';
+    const CLIENT_SECRET_MANDIRI = '0bc563d8-ff76-4cd3-90ae-f79e5d704625';
     const PATNER_ID_MANDIRI = 'UATCORPAY';
     const PRIVATE_KEY = 'RqCZ4teYVi9eI+I81oN18BlnVl7wlXHW7uF8z2tiSFM=';
     const RSA_TYPE = OPENSSL_ALGO_SHA256;
